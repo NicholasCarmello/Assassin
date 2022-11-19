795a7c76ab8f1717ed9d52360555b5b51734186a
nicholascarmello <nick.carmello@yahoo.com>
2022-11-19 09:07:21 -0500

    Initial Commit

---
+++ Shared/AssassinApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssassinApp.swift
+//  Shared
+//
+//  Created by Nicholas Carmello on 11/19/22.
+//
+
+import SwiftUI
+
+@main
+struct AssassinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
